ac3f90e095352d73b5c8eada2ed45b0d8ab5511b
Gianni Ballerini <gianniballerini@gmail.com>
2020-09-22 20:10:00 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, FlatList, Pressable, Alert } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [countriesData, setCountriesData] = useState([])
+
+  function fetchCountriesData() {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=60')
+      .then((response) => response.json())
+      .then((json) => setCountriesData(json.results))
+      .catch((error) => console.error(error))
+  }
+
+  useEffect(()=> {
+    fetchCountriesData();
+  })
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='ligth'/>
+      <FlatList
+        data={countriesData}
+        contentContainerStyle={styles.container}
+        keyExtractor={item => item.name}
+        renderItem={({item})=> <Text onPress={() => {Alert.alert(`The URL of ${item.name} is ${item.url}`)}} style={styles.text}>{item.name}</Text>}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: 'flex',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
 });
