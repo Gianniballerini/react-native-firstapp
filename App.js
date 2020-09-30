aad19deb12bab7dc0021aaa1e7b8df302dedc7a4
Gianni Ballerini <gianniballerini@gmail.com>
2020-09-30 20:05:37 -0300

    Selection is now working, and the api call is only made once. It was repeating before

--- App.js
+++ App.js
@@ -1,38 +1,67 @@
-import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, FlatList, Pressable, Alert } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from "react";
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 export default function App() {
-  const [countriesData, setCountriesData] = useState([])
+  const [pokemonData, setPokemonData] = useState([])
+  const [selectedId, setSelectedId] = useState(null);
 
-  function fetchCountriesData() {
-    fetch('https://pokeapi.co/api/v2/pokemon/?limit=60')
-      .then((response) => response.json())
-      .then((json) => setCountriesData(json.results))
-      .catch((error) => console.error(error))
+  async function fetchPokemonData() {
+    try {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=60')
+      let json = await response.json()
+      console.log(json.results)
+      setPokemonData(json.results)
+    } catch(error) {
+      console.error(error);
+    }
   }
 
-  useEffect(()=> {
-    fetchCountriesData();
-  })
-  
-  return (
-    <>
-      <StatusBar style='ligth'/>
-      <FlatList
-        data={countriesData}
-        contentContainerStyle={styles.container}
-        keyExtractor={item => item.name}
-        renderItem={({item})=> <Text onPress={() => {Alert.alert(`The URL of ${item.name} is ${item.url}`)}} style={styles.text}>{item.name}</Text>}
+  const Item = ({ item, onPress, style }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
+      <Text style={styles.title}>{item.name}</Text>
+    </TouchableOpacity>
+  );
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.name === selectedId ? "#6e3b6e" : "#f9c2ff";
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.name)}
+        style={{ backgroundColor }}
       />
-    </>
+    );
+  };
+
+  
+
+  useEffect(() => {
+    ()  => fetchPokemonData();
+  })
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={pokemonData}
+        renderItem={renderItem}
+        keyExtractor={item => item.name}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    backgroundColor: '#fff',
-    alignItems: 'center'
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
